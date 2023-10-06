5c31b7d98bef2b4235862b3f807ab51ac2562266
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-10-06 19:19:43 +0100

    updated design - new

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -30,7 +30,7 @@ import { DiDotnet } from "react-icons/di";
 import { TbExchange } from "react-icons/tb";
 import { FaRobot } from "react-icons/fa";
 
-import skillsImage from "../assets/skills-image.png";
+import mySkillsImg from "../assets/my-skills.png";
 
 const MySkills = ({ setSelectedPage }) => {
   const isAboveMediumLargeScreens = useMediaQuery("(min-width:1300px)");
@@ -38,10 +38,10 @@ const MySkills = ({ setSelectedPage }) => {
   return (
     <section
       id="skills"
-      className="gap-16 py-10 min-h-[100dvh] flex flex-col justify-center"
+      className="gap-16 py-10 min-h-[100dvh] flex flex-col justify-center z-10"
     >
       {/* FIRST ROW */}
-      <div className="md:flex justify-between md:text-start text-center md:items-start mt-32 gap-20">
+      <div className="md:flex justify-between xl:text-end md:text-start text-center md:items-start mt-32 gap-20">
         <motion.div
           className="lg:w-1/3 md:w-1/2 "
           initial="hidden"
@@ -55,49 +55,54 @@ const MySkills = ({ setSelectedPage }) => {
           onViewportEnter={() => setSelectedPage("skills")}
         >
           {/* /MY SKILLS COLUMN */}
-          <p className="font-playfair mb-2 font-semibold text-4xl">
+          <p className="font-title mb-2 font-black text-5xl">
             MY <span className="text-primary-2 ">SKILLS</span>{" "}
           </p>{" "}
-          <div className="flex justify-center md:justify-start my-5">
+          <div className="flex justify-center md:justify-start xl:justify-end my-5">
             <LineGradient width="w-1/4" />
           </div>
-          <p className="font-extralight text-sm mt-5 mb-10  drop-shadow-accent ">
-            My background includes a bachelor's degree in mechanical
-            engineering, which provided me with a deep understanding of design
-            and problem-solving. Furthermore, I have honed my{" "}
-            <span className="text-primary-light-2 ">creativity</span> as a
-            talented musician, having taught drums for Yamaha Music Europe. With
-            <span className="text-primary-light-2"> experience </span>
-            as a customer advisor in a finance company and currently working as
-            an assistant manager in a hotel, I have excellent{" "}
-            <span className="text-primary-light-2">
-              communication skills{" "}
-            </span>{" "}
-            and I am great with customers.
-            <br /> I am confident that my combination of technical{" "}
-            <span className="text-primary-light-2">knowledge</span> and
-            creativity, as well as my ability to communicate effectively, make
-            me a <span className="text-primary-light-2">strong candidate</span>{" "}
-            for junior web development position.
-          </p>
+          <div className="mt-5 mb-10 text-lg drop-shadow-accent ">
+            <p className="font-bold">Engineering Background</p>
+            <p className="mb-2 text-primary-light-1 text-sm font-thin">
+              B.Eng - Aeronautics and Space Technology,<br></br> Major in
+              Aricraft Engines
+            </p>
+            <p className="font-bold">Creativity & Communication</p>
+            <p className="mb-2 text-primary-light-1 text-sm">
+              Yamaha Music Europe Drum Teacher
+            </p>
+            <p className="font-bold">Communication & Issue Resolution</p>
+            <p className="mb-4 text-primary-light-1 text-sm">
+              Hospitality Assistance Manger, <br></br>Client Relations
+              Consultant
+            </p>
+          </div>
         </motion.div>
         {/* /IMAGE */}
         <div
-          className={isAboveMediumLargeScreens ? "flex-grow-1" : "w-50 m-auto"}
+          className={
+            isAboveMediumLargeScreens ? "mr-auto w-7/12" : "w-50 m-auto"
+          }
         >
           {isAboveMediumScreens ? (
             <motion.div>
-              <img src={skillsImage} alt="" className="mx-auto " />
+              <div className="relative z-0 ml-20 before:absolute before:scale-y-[70%] xl:before:scale-x-[110%] before:scale-x-[85%] before:top-[2%] before:-left-[7%] xl:before:left-[10%] xxl:before:left-[25%] xxxl:before:left-[25%] before:w-full before:max-w-[400px] before:h-full before:max-h-[400px] md:before:max-w-[600px] md:before:max-h-[600px] before:border-2 before:border-primary-2 before:z-[-1]">
+                <img
+                  src={mySkillsImg}
+                  alt=""
+                  className="mx-auto w-full max-w-[400px] md:max-w-[1000px]"
+                />
+              </div>
             </motion.div>
           ) : (
             <motion.div>
-              <img src={skillsImage} alt="" className="mx-auto" />
+              <img src={mySkillsImg} alt="" className="mx-auto h-fit" />
             </motion.div>
           )}
         </div>
       </div>
       {/* SKILLS */}
-      <section className="md:flex justify-between md:items-start md:h-full mt-32 md:gap-16 text-sm">
+      <section className="md:flex justify-between md:items-start md:h-full mt-32 md:gap-16 text-sm z-10">
         <motion.div
           initial="hidden"
           whileInView="visible"
@@ -109,9 +114,9 @@ const MySkills = ({ setSelectedPage }) => {
           }}
           className="md:w-1/4 mb-16"
         >
-          <div className="pb-2 relative before:absolute before:h-full before:bg-primary-1 before:z-[-1] before:w-1/2 before:left-1/2 md:before:w-3/4 md:before:left-1/4">
-            <p className="font-semibold text-4xl mb-2">01</p>
-            <p className="font-semibold text-4xl mb-2 drop-shadow-accent">
+          <div className="pb-2 relative before:absolute before:h-full before:border-primary-1 before:border-2 before:z-[-1] before:w-1/2 before:left-1/2 md:before:w-3/4 md:before:left-1/4">
+            <p className="font-bold font-title text-4xl mb-2">01</p>
+            <p className="font-bold font-title text-4xl mb-2 drop-shadow-accent">
               <span className="md:text-primary-light-1 text-primary-1 ">
                 Front
               </span>{" "}
@@ -165,9 +170,9 @@ const MySkills = ({ setSelectedPage }) => {
           }}
           className="md:w-1/4 mb-16"
         >
-          <div className="pb-2 relative before:absolute before:h-full before:bg-primary-2 before:z-[-1] before:w-1/2 before:left-1/2 md:before:w-3/4 md:before:left-1/4">
-            <p className=" font-semibold text-4xl mb-2">02</p>
-            <p className="font-semibold text-4xl mb-2 drop-shadow-accent ">
+          <div className="pb-2 relative before:absolute before:h-full before:border-primary-2 before:border-2 before:z-[-1] before:w-1/2 before:left-1/2 md:before:w-3/4 md:before:left-1/4">
+            <p className=" font-bold font-title text-4xl mb-2">02</p>
+            <p className="font-bold font-title text-4xl mb-2 drop-shadow-accent ">
               <span className="md:text-primary-light-2 text-primary-2 ">
                 Back
               </span>{" "}
@@ -225,9 +230,9 @@ const MySkills = ({ setSelectedPage }) => {
           }}
           className="md:w-1/4 mb-16"
         >
-          <div className="pb-2 relative before:absolute before:h-full before:bg-primary-3 before:z-[-1] before:w-1/2 before:left-1/2 md:before:w-3/4 md:before:left-1/4">
-            <p className="font-semibold text-4xl mb-2">03</p>
-            <p className="font-semibold text-4xl mb-2 drop-shadow-accent ">
+          <div className="pb-2 relative before:absolute before:h-full before:border-primary-3 before:border-2 before:z-[-1] before:w-1/2 before:left-1/2 md:before:w-3/4 md:before:left-1/4">
+            <p className="font-bold font-title text-4xl mb-2">03</p>
+            <p className="font-bold font-title text-4xl mb-2 drop-shadow-accent ">
               <span className="md:text-primary-light-3 text-primary-3">
                 And
               </span>{" "}
